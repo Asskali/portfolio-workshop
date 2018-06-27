9902bc661050605151bac8fbd0b4a6cef9e52fff
Adam Asskali <adam.asskali@gmail.com>
2018-06-27 20:06:23 +0200

    Fullført grunndelen av nettsiden

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { LandingPage } from './components/pages';
+import { ScrollTop } from './components/scroll';
+import { AboutPage } from './components/pages';
+import { PortFolioPage } from './components/pages';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="landing-page">
+        <LandingPage/>
+        <AboutPage/>
+        <PortFolioPage/>
+        <ScrollTop/>
       </div>
     );
   }
